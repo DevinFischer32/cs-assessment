95a3f662d9f3e012ca198f55510742ccee13e1d0
Devin Fischer <devinfischer32@gmail.com>
2021-09-03 11:39:27 -0600

    part 3

---
+++ Step3.js
@@ -0,0 +1,24 @@
+/* 
+3) Pangram Sentence
+A pangram is a sentence that contains all the letters of the English alphabet at least once, like “The quick brown fox jumps over the lazy dog.”
+
+Write a function to check a sentence to see if it is a pangram or not.
+
+For example:
+
+isPangram("The quick brown fox jumps over the lazy dog!");
+// -> True
+
+isPangram("I like cats, but not mice");
+// -> False
+
+*/
+function isPangram(string){
+    const alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+          'v', 'w', 'x', 'y', 'z'];
+      string = string.toLowerCase();
+      for(let i = 0; i < alpha.length; i++) {
+           if ((string.indexOf(alpha[i])  === -1)) return false;
+     }
+      return true;
+}
